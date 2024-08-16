8d3866717b2a1297b97db455cd0fe51109774f75
DevAlefhiLima <devalefhilima@gmail.com>
2024-08-15 23:18:33 -0400

    Adicionando a funcionalidade de Deletar Item

--- script.js
+++ script.js
@@ -17,18 +17,24 @@ function adicionarNovaTarefa() {
 function mostrarTarefas() {
     let novaLi = ""
 
-    minhaListaDeItens.forEach(tarefa => {
+    minhaListaDeItens.forEach((tarefa, index) => {
         novaLi = novaLi + `
             <li class="task">
             <img src="./img/checked.png" alt="Check na tarefa">
             <p>${tarefa}</p>
-            <img src="./img/trash.png" alt="tarefa para o lixo">
+            <img src="./img/trash.png" alt="tarefa para o lixo" onclick="deletarItem(${index})">
             </li>
         `
     })
 
     listaCompleta.innerHTML = novaLi
 
+}
+
+function deletarItem(index) {
+    minhaListaDeItens.splice(index, 1)
+
+    mostrarTarefas()
 }
 
 button.addEventListener("click", adicionarNovaTarefa)
