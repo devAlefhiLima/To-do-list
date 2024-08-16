bca54fadbfcbe3291d767c36fd92cffe7a848b7c
DevAlefhiLima <devalefhilima@gmail.com>
2024-08-15 23:10:00 -0400

    adicionando a função de mostrar tarefas e mostrando as tarefas no html

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector(".button-add-task")
 const input = document.querySelector(".input-task")
+const listaCompleta = document.querySelector(".list-tasks")
 
 let minhaListaDeItens = []
 
@@ -8,9 +9,26 @@ let minhaListaDeItens = []
 function adicionarNovaTarefa() {
     minhaListaDeItens.push(input.value)
 
-    console.log(minhaListaDeItens)
+    input.value = ""
+
+    mostrarTarefas()
 }
 
+function mostrarTarefas() {
+    let novaLi = ""
 
+    minhaListaDeItens.forEach(tarefa => {
+        novaLi = novaLi + `
+            <li class="task">
+            <img src="./img/checked.png" alt="Check na tarefa">
+            <p>${tarefa}</p>
+            <img src="./img/trash.png" alt="tarefa para o lixo">
+            </li>
+        `
+    })
+
+    listaCompleta.innerHTML = novaLi
+
+}
 
 button.addEventListener("click", adicionarNovaTarefa)
